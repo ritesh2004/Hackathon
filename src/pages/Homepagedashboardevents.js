5b825e22af1d851c96e1cde8e7f91f0a669cbdf8
Myselfsayan <sayanghosh8702@gmail.com>
2023-07-09 11:25:45 +0530

    2nd page

---
+++ src/pages/Homepagedashboardevents.js
@@ -0,0 +1,372 @@
+import "./Homepagedashboardevents.css";
+const Homepagedashboardevents = () => {
+  return (
+    <div className="homepagedashboardevents">
+      <a className="homepagedashboardevents-inner">
+        <div className="event-hive-parent">
+          <b className="event-hive">Event Hive</b>
+          <div className="frame-parent">
+            <a className="frame-wrapper" target="_blank">
+              <div className="phosphor-icons-squaresfour-parent">
+                <img
+                  className="phosphor-icons-squaresfour"
+                  alt=""
+                  src="/phosphor-icons--squaresfour.svg"
+                />
+                <b className="event-hive">Dashboard</b>
+              </div>
+            </a>
+            <a className="phosphor-icons-televisionsim-parent" target="_blank">
+              <img
+                className="phosphor-icons-squaresfour"
+                alt=""
+                src="/phosphor-icons--televisionsimple.svg"
+              />
+              <b className="event-hive">Events</b>
+            </a>
+            <div className="phosphor-icons-paperplanerig-parent">
+              <img
+                className="phosphor-icons-squaresfour"
+                alt=""
+                src="/phosphor-icons--paperplaneright.svg"
+              />
+              <b className="event-hive">Messages</b>
+            </div>
+            <a className="phosphor-icons-user-parent" target="_blank">
+              <img
+                className="phosphor-icons-squaresfour"
+                alt=""
+                src="/phosphor-icons--user.svg"
+              />
+              <b className="event-hive">Profile</b>
+            </a>
+          </div>
+        </div>
+      </a>
+      <div className="homepagedashboardevents-child">
+        <div className="group-child" />
+      </div>
+      <div className="frame-group">
+        <div className="large-full-image-wrapper">
+          <div className="large-full-image">
+            <div className="background-image">
+              <img
+                className="products-2-icon"
+                alt=""
+                src="/products-2@2x.png"
+              />
+            </div>
+            <div className="text">
+              <b className="title">
+                Discover and experience extraordinary Events
+              </b>
+              <div className="bodytext">
+                Enter in the world of events. Discover now the latest Events or
+                start creating your own!
+              </div>
+            </div>
+            <div className="later-button">
+              <div className="later">Watch video</div>
+            </div>
+            <div className="button">
+              <div className="button1">
+                <div className="background">
+                  <div className="background-child" />
+                </div>
+                <div className="text1">
+                  <div className="large-button">Discover now</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="events-participated-wrapper">
+          <b className="events-participated">
+            <span>Events</span>
+            <span className="participated"> Participated</span>
+          </b>
+        </div>
+        <div className="medium-location-parent">
+          <div className="medium-location">
+            <div className="medium-button">
+              <div className="background">
+                <div className="background-item" />
+              </div>
+              <div className="text2">
+                <div className="medium-button1">Register</div>
+              </div>
+            </div>
+            <b className="bid">Current Data</b>
+            <div className="avatars-group-1-horizontal">
+              <img className="avatar-1-icon" alt="" src="/avatar-1@2x.png" />
+              <img className="avatar-2-icon" alt="" src="/avatar-2@2x.png" />
+              <img className="avatar-3-icon" alt="" src="/avatar-3@2x.png" />
+              <div className="avatar-4">
+                <div className="avatar-4-child" />
+                <b className="b">18+</b>
+              </div>
+            </div>
+            <div className="text3">
+              <b className="content-title">Abstract Colors</b>
+              <div className="tag">By Esthera Jackson</div>
+            </div>
+            <div className="photo">
+              <div className="card">
+                <img
+                  className="fakurian-design-kakvfqbniyi-un-icon"
+                  alt=""
+                  src="/fakuriandesignkakvfqbniyiunsplash-1@2x.png"
+                />
+                <div className="badge-stats-favorite">
+                  <div className="background2">
+                    <div className="background-inner" />
+                  </div>
+                  <img
+                    className="favorite-icon"
+                    alt=""
+                    src="/favorite-icon.svg"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="medium-location">
+            <div className="medium-button">
+              <div className="background">
+                <div className="background-item" />
+              </div>
+              <div className="text2">
+                <div className="medium-button1">Register</div>
+              </div>
+            </div>
+            <b className="bid">Current Data</b>
+            <div className="avatars-group-1-horizontal">
+              <img className="avatar-1-icon" alt="" src="/avatar-1@2x.png" />
+              <img className="avatar-2-icon" alt="" src="/avatar-2@2x.png" />
+              <img className="avatar-3-icon" alt="" src="/avatar-3@2x.png" />
+              <div className="avatar-4">
+                <div className="avatar-4-child" />
+                <b className="b">18+</b>
+              </div>
+            </div>
+            <div className="text3">
+              <b className="content-title">Abstract Colors</b>
+              <div className="tag">By Esthera Jackson</div>
+            </div>
+            <div className="photo">
+              <div className="card">
+                <img
+                  className="fakurian-design-kakvfqbniyi-un-icon"
+                  alt=""
+                  src="/fakuriandesignkakvfqbniyiunsplash-1@2x.png"
+                />
+                <div className="badge-stats-favorite">
+                  <div className="background2">
+                    <div className="background-inner" />
+                  </div>
+                  <img
+                    className="favorite-icon"
+                    alt=""
+                    src="/favorite-icon.svg"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="medium-location">
+            <div className="medium-button">
+              <div className="background">
+                <div className="background-item" />
+              </div>
+              <div className="text2">
+                <div className="medium-button1">Register</div>
+              </div>
+            </div>
+            <b className="bid">Current Data</b>
+            <div className="avatars-group-1-horizontal">
+              <img className="avatar-1-icon" alt="" src="/avatar-1@2x.png" />
+              <img className="avatar-2-icon" alt="" src="/avatar-2@2x.png" />
+              <img className="avatar-3-icon" alt="" src="/avatar-3@2x.png" />
+              <div className="avatar-4">
+                <div className="avatar-4-child" />
+                <b className="b">18+</b>
+              </div>
+            </div>
+            <div className="text3">
+              <b className="content-title">Abstract Colors</b>
+              <div className="tag">By Esthera Jackson</div>
+            </div>
+            <div className="photo">
+              <div className="card">
+                <img
+                  className="fakurian-design-kakvfqbniyi-un-icon"
+                  alt=""
+                  src="/fakuriandesignkakvfqbniyiunsplash-1@2x.png"
+                />
+                <div className="badge-stats-favorite">
+                  <div className="background2">
+                    <div className="background-inner" />
+                  </div>
+                  <img
+                    className="favorite-icon"
+                    alt=""
+                    src="/favorite-icon.svg"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="trending-events-parent">
+          <b className="trending-events">Trending Events</b>
+          <div className="badge-tag-parent">
+            <div className="badge-tag">
+              <div className="background">
+                <div className="background-inner" />
+              </div>
+              <div className="text8">
+                <div className="badge">Technical</div>
+              </div>
+            </div>
+            <div className="music">Music</div>
+            <div className="cultural">Cultural</div>
+            <div className="sports">Sports</div>
+          </div>
+        </div>
+        <div className="medium-location-parent">
+          <div className="medium-location">
+            <div className="medium-button">
+              <div className="background">
+                <div className="background-item" />
+              </div>
+              <div className="text2">
+                <div className="medium-button1">Register</div>
+              </div>
+            </div>
+            <b className="bid">Current Data</b>
+            <div className="avatars-group-1-horizontal">
+              <img className="avatar-1-icon" alt="" src="/avatar-1@2x.png" />
+              <img className="avatar-2-icon" alt="" src="/avatar-2@2x.png" />
+              <img className="avatar-3-icon" alt="" src="/avatar-3@2x.png" />
+              <div className="avatar-4">
+                <div className="avatar-4-child" />
+                <b className="b">18+</b>
+              </div>
+            </div>
+            <div className="text3">
+              <b className="content-title">Abstract Colors</b>
+              <div className="tag">By Esthera Jackson</div>
+            </div>
+            <div className="photo">
+              <div className="card">
+                <img
+                  className="fakurian-design-kakvfqbniyi-un-icon"
+                  alt=""
+                  src="/fakuriandesignkakvfqbniyiunsplash-1@2x.png"
+                />
+                <div className="badge-stats-favorite">
+                  <div className="background2">
+                    <div className="background-inner" />
+                  </div>
+                  <img
+                    className="favorite-icon"
+                    alt=""
+                    src="/favorite-icon.svg"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="medium-location">
+            <div className="medium-button">
+              <div className="background">
+                <div className="background-item" />
+              </div>
+              <div className="text2">
+                <div className="medium-button1">Register</div>
+              </div>
+            </div>
+            <b className="bid">Current Data</b>
+            <div className="avatars-group-1-horizontal">
+              <img className="avatar-1-icon" alt="" src="/avatar-1@2x.png" />
+              <img className="avatar-2-icon" alt="" src="/avatar-2@2x.png" />
+              <img className="avatar-3-icon" alt="" src="/avatar-3@2x.png" />
+              <div className="avatar-4">
+                <div className="avatar-4-child" />
+                <b className="b">18+</b>
+              </div>
+            </div>
+            <div className="text3">
+              <b className="content-title">Abstract Colors</b>
+              <div className="tag">By Esthera Jackson</div>
+            </div>
+            <div className="photo">
+              <div className="card">
+                <img
+                  className="fakurian-design-kakvfqbniyi-un-icon"
+                  alt=""
+                  src="/fakuriandesignkakvfqbniyiunsplash-1@2x.png"
+                />
+                <div className="badge-stats-favorite">
+                  <div className="background2">
+                    <div className="background-inner" />
+                  </div>
+                  <img
+                    className="favorite-icon"
+                    alt=""
+                    src="/favorite-icon.svg"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="medium-location">
+            <div className="medium-button">
+              <div className="background">
+                <div className="background-item" />
+              </div>
+              <div className="text2">
+                <div className="medium-button1">Register</div>
+              </div>
+            </div>
+            <b className="bid">Current Data</b>
+            <div className="avatars-group-1-horizontal">
+              <img className="avatar-1-icon" alt="" src="/avatar-1@2x.png" />
+              <img className="avatar-2-icon" alt="" src="/avatar-2@2x.png" />
+              <img className="avatar-3-icon" alt="" src="/avatar-3@2x.png" />
+              <div className="avatar-4">
+                <div className="avatar-4-child" />
+                <b className="b">18+</b>
+              </div>
+            </div>
+            <div className="text3">
+              <b className="content-title">Abstract Colors</b>
+              <div className="tag">By Esthera Jackson</div>
+            </div>
+            <div className="photo">
+              <div className="card">
+                <img
+                  className="fakurian-design-kakvfqbniyi-un-icon"
+                  alt=""
+                  src="/fakuriandesignkakvfqbniyiunsplash-1@2x.png"
+                />
+                <div className="badge-stats-favorite">
+                  <div className="background2">
+                    <div className="background-inner" />
+                  </div>
+                  <img
+                    className="favorite-icon"
+                    alt=""
+                    src="/favorite-icon.svg"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepagedashboardevents;
