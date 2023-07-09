02476a6b8b28dc7ec80300c2a745f08a1c4a4b4f
Myselfsayan <sayanghosh8702@gmail.com>
2023-07-09 12:11:59 +0530

    Event_page_2

---
+++ src/pages/HomepagecollegeEvents.js
@@ -0,0 +1,269 @@
+import "./HomepagecollegeEvents.css";
+const HomepagecollegeEvents = () => {
+  return (
+    <div className="homepagecollege-events">
+      <div className="event-hive-parent">
+        <b className="event-hive">
+          <span>Event</span>
+          <span className="hive"> Hive</span>
+        </b>
+        <div className="login-parent">
+          <div className="event-hive">Login</div>
+          <div className="button">
+            <div className="event-hive">Signup</div>
+          </div>
+        </div>
+      </div>
+      <div className="frame-parent">
+        <div className="event-parent">
+          <div className="event">
+            <div className="event1">
+              <img
+                className="eventimage3-icon"
+                alt=""
+                src="/eventimage3@2x.png"
+              />
+            </div>
+            <div className="free-wrapper">
+              <div className="event-hive">FREE</div>
+            </div>
+            <div className="bestselller-book-bootcamp">{`BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow`}</div>
+            <div className="saturdat-march-18">Saturdat, March 18, 9.30PM</div>
+            <div className="online-event-">ONLINE EVENT - Attend anywhere</div>
+          </div>
+          <div className="event">
+            <div className="event1">
+              <img
+                className="eventimage3-icon"
+                alt=""
+                src="/unsplash1dw1vejllcq@2x.png"
+              />
+            </div>
+            <div className="free-wrapper">
+              <div className="event-hive">FREE</div>
+            </div>
+            <div className="bestselller-book-bootcamp">{`BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow`}</div>
+            <div className="saturdat-march-18">Saturdat, March 18, 9.30PM</div>
+            <div className="online-event-">ONLINE EVENT - Attend anywhere</div>
+          </div>
+          <div className="event">
+            <div className="event1">
+              <img
+                className="eventimage3-icon"
+                alt=""
+                src="/unsplashqfwmuxdcn18@2x.png"
+              />
+            </div>
+            <div className="free-wrapper">
+              <div className="event-hive">FREE</div>
+            </div>
+            <div className="bestselller-book-bootcamp">{`BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow`}</div>
+            <div className="saturdat-march-18">Saturdat, March 18, 9.30PM</div>
+            <div className="online-event-">ONLINE EVENT - Attend anywhere</div>
+          </div>
+        </div>
+        <div className="event-parent">
+          <div className="event">
+            <div className="event1">
+              <img
+                className="eventimage3-icon"
+                alt=""
+                src="/unsplashqfwmuxdcn181@2x.png"
+              />
+            </div>
+            <div className="free-wrapper">
+              <div className="event-hive">FREE</div>
+            </div>
+            <div className="bestselller-book-bootcamp">{`BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow`}</div>
+            <div className="saturdat-march-18">Saturdat, March 18, 9.30PM</div>
+            <div className="online-event-">ONLINE EVENT - Attend anywhere</div>
+          </div>
+          <div className="event">
+            <div className="event1">
+              <img
+                className="eventimage3-icon"
+                alt=""
+                src="/unsplashqfwmuxdcn182@2x.png"
+              />
+            </div>
+            <div className="free-wrapper">
+              <div className="event-hive">FREE</div>
+            </div>
+            <div className="bestselller-book-bootcamp">{`BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow`}</div>
+            <div className="saturdat-march-18">Saturdat, March 18, 9.30PM</div>
+            <div className="online-event-">ONLINE EVENT - Attend anywhere</div>
+          </div>
+          <div className="event">
+            <div className="event1">
+              <img
+                className="eventimage3-icon"
+                alt=""
+                src="/eventimage31@2x.png"
+              />
+            </div>
+            <div className="free-wrapper">
+              <div className="event-hive">FREE</div>
+            </div>
+            <div className="bestselller-book-bootcamp">{`BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow`}</div>
+            <div className="saturdat-march-18">Saturdat, March 18, 9.30PM</div>
+            <div className="online-event-">ONLINE EVENT - Attend anywhere</div>
+          </div>
+        </div>
+        <div className="event-parent">
+          <div className="event">
+            <div className="event1">
+              <img
+                className="eventimage3-icon"
+                alt=""
+                src="/unsplashqfwmuxdcn181@2x.png"
+              />
+            </div>
+            <div className="free-wrapper">
+              <div className="event-hive">FREE</div>
+            </div>
+            <div className="bestselller-book-bootcamp">{`BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow`}</div>
+            <div className="saturdat-march-18">Saturdat, March 18, 9.30PM</div>
+            <div className="online-event-">ONLINE EVENT - Attend anywhere</div>
+          </div>
+          <div className="event">
+            <div className="event1">
+              <img
+                className="eventimage3-icon"
+                alt=""
+                src="/unsplashqfwmuxdcn182@2x.png"
+              />
+            </div>
+            <div className="free-wrapper">
+              <div className="event-hive">FREE</div>
+            </div>
+            <div className="bestselller-book-bootcamp">{`BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow`}</div>
+            <div className="saturdat-march-18">Saturdat, March 18, 9.30PM</div>
+            <div className="online-event-">ONLINE EVENT - Attend anywhere</div>
+          </div>
+          <div className="event">
+            <div className="event1">
+              <img
+                className="eventimage3-icon"
+                alt=""
+                src="/eventimage31@2x.png"
+              />
+            </div>
+            <div className="free-wrapper">
+              <div className="event-hive">FREE</div>
+            </div>
+            <div className="bestselller-book-bootcamp">{`BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow`}</div>
+            <div className="saturdat-march-18">Saturdat, March 18, 9.30PM</div>
+            <div className="online-event-">ONLINE EVENT - Attend anywhere</div>
+          </div>
+        </div>
+      </div>
+      <div className="colleges-near-you-parent">
+        <b className="colleges-near-you-container">
+          <span>Colleges</span>
+          <span className="near-you"> near you</span>
+        </b>
+        <div className="accordion-parent">
+          <div className="accordion">
+            <div className="event-hive">Weekdays</div>
+            <img
+              className="fether-icons-chevron-down"
+              alt=""
+              src="/fether-icons--chevrondown.svg"
+            />
+          </div>
+          <div className="accordion">
+            <div className="event-hive">Event type</div>
+            <img
+              className="fether-icons-chevron-down"
+              alt=""
+              src="/fether-icons--chevrondown.svg"
+            />
+          </div>
+          <div className="accordion">
+            <div className="event-hive">Any category</div>
+            <img
+              className="fether-icons-chevron-down"
+              alt=""
+              src="/fether-icons--chevrondown.svg"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="unsplashyrmwvcdyhmi-parent">
+        <img
+          className="unsplashyrmwvcdyhmi-icon"
+          alt=""
+          src="/unsplashyrmwvcdyhmi@2x.png"
+        />
+        <img
+          className="unsplashucr4yp-t364-icon"
+          alt=""
+          src="/unsplashucr4ypt364@2x.png"
+        />
+        <img
+          className="unsplashyrmwvcdyhmi-icon"
+          alt=""
+          src="/unsplashu0dbv-qeiyk@2x.png"
+        />
+      </div>
+      <b className="checkout-your-college-container">
+        <span>{`Checkout your `}</span>
+        <span className="hive">college events</span>
+        <span> now!!</span>
+      </b>
+      <div className="rectangle-parent">
+        <div className="group-child" />
+        <b className="event-hive1">
+          <span>Event</span>
+          <span className="span">{` `}</span>
+          <span className="hive">Hive</span>
+        </b>
+        <div className="input-parent">
+          <div className="input">
+            <div className="event-hive">Enter your mail</div>
+          </div>
+          <div className="button1">
+            <div className="event-hive">Subscribe</div>
+          </div>
+        </div>
+        <div className="home-parent">
+          <div className="home">Home</div>
+          <div className="home">About</div>
+          <div className="services">Services</div>
+          <div className="services">Get in touch</div>
+          <div className="home">FAQs</div>
+        </div>
+        <div className="group-item" />
+        <div className="non-copyrighted">
+          Non Copyrighted © 2023 Upload by rich technologies
+        </div>
+        <div className="phosphor-icons-linkedinlogo-parent">
+          <img
+            className="phosphor-icons-linkedinlogo"
+            alt=""
+            src="/phosphor-icons--linkedinlogo.svg"
+          />
+          <img
+            className="phosphor-icons-linkedinlogo"
+            alt=""
+            src="/phosphor-icons--instagramlogo.svg"
+          />
+          <img
+            className="phosphor-icons-linkedinlogo"
+            alt=""
+            src="/phosphor-icons--facebooklogo.svg"
+          />
+        </div>
+        <div className="button-parent">
+          <div className="button2">
+            <div className="event-hive">English</div>
+          </div>
+          <div className="event-hive">French</div>
+          <div className="event-hive">Hindi</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomepagecollegeEvents;
